4411681deca1ef3ca6cf9a204b0699f75f6c1f9d
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-19 20:24:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
-module "storage_accounts" {
-  source               = "./module_storage_account/"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  storage_accounts     = {
+locals {
+  resource_group_name = "rg-terraform"
+  location            = "East US"
+
+  storage_accounts = {
     "sa1" = { name = "storageaccount01", account_tier = "Standard", replication = "LRS" }
     "sa2" = { name = "storageaccount02", account_tier = "Premium", replication = "GRS" }
   }
+}
+
+module "storage_accounts" {
+  source               = "./module_storage_account/"
+  resource_group_name  = local.resource_group_name
+  location             = local.location
+  storage_accounts     = local.storage_accounts
 }
