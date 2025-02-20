974137b57552f96aec8a85dbc8910bac8dcce5ae
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-19 20:27:57 -0500

    Update variable.tf

--- module_storage_account/variable.tf
+++ module_storage_account/variable.tf
@@ -1,8 +1,8 @@
 variable "storage_accounts" {
   type = map(object({
-    name            = string
-    account_tier    = string
-    replication     = string
+    name        = string
+    account_tier = string
+    replication = string
   }))
   description = "Liste des comptes de stockage à créer"
 }
