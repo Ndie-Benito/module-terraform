e30c01ef6cd595c74652da1cd57c4dcac1deba74
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-19 19:59:39 -0500

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals{
+  storage_account_name  = "benitostorageaccount"
+  resource_group_name   = "benitoresourcegroup"
+  location              = "eastus"
+  account_tier          = "Standard"
+}
