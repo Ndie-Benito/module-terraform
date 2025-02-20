7c1f0f8be0be7cff88e6e0475124c53f262e1d01
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-19 20:36:22 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
-locals{
+/*locals{
   storage_account_name  = "benitostorageaccount"
   resource_group_name   = "benitoresourcegroup"
   location              = "eastus"
   account_tier          = "Standard"
 }
+*/
